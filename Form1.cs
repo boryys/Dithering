19e8aa58a63e8de17491afa4f95f73433d86008d
UlaBorys <ula_borys@op.pl>
2018-03-15 14:55:18 +0100

    picture changes

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,13 @@ namespace Dithering
     public partial class Form1 : Form
     {
         Bitmap originalPhoto;
+        Bitmap grayScaledPhoto;
         
         public Form1()
         {
             InitializeComponent();
+            pictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             matrix.Enabled = false;
             kBox.Text = "2";
             matrix.Text = "F&S";
@@ -26,6 +29,7 @@ namespace Dithering
         {
             Color color;
             double c;
+            grayScaledPhoto = (Bitmap)originalPhoto.Clone();
 
             for (int x = 0; x < originalPhoto.Width; x++)
             {
@@ -35,11 +39,11 @@ namespace Dithering
 
                     c = 0.3 * color.R + 0.6 * color.G + 0.1 * color.B;
 
-                    originalPhoto.SetPixel(x, y, Color.FromArgb((int)c, (int)c, (int)c));
+                    grayScaledPhoto.SetPixel(x, y, Color.FromArgb((int)c, (int)c, (int)c));
                 }
             }
 
-            pictureBox.Image = originalPhoto;
+            pictureBox.Image = grayScaledPhoto;
         }
 
         private Bitmap randomFilter(int k)
@@ -207,16 +211,76 @@ namespace Dithering
                     int th1 = r.Next(0, 256);
                     int th2 = r.Next(0, th1 + 1);
                     int th3 = r.Next(th1, 256);
+                    int th21 = r.Next(0, th2 + 1);
+                    int th22 = r.Next(th2, th1 + 1);
+                    int th31 = r.Next(th1, th3 + 1);
+                    int th32 = r.Next(th3, 256);
+                    int th211 = r.Next(0, th21 + 1);
+                    int th212 = r.Next(th21, th2 + 1);
+                    int th221 = r.Next(th2, th22 + 1);
+                    int th222 = r.Next(th22, th1 + 1);
+                    int th311 = r.Next(th1, th31 + 1);
+                    int th312 = r.Next(th31, th3 + 1);
+                    int th321 = r.Next(th3, th32 + 1);
+                    int th322 = r.Next(th32, 256);
 
                     if (c > th1)
                     {
-                        if (c > th3) tmp.SetPixel(x, y, Color.FromArgb((int)w, (int)w, (int)w));
-                        else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 2 / 3), (int)(w * 2 / 3), (int)(w * 2 / 3)));
+                        if (c > th3)
+                        {
+                            if (c > th32)
+                            {
+                                if (c > th322) tmp.SetPixel(x, y, Color.FromArgb((int)w, (int)w, (int)w));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 14 / 15), (int)(w * 14 / 15), (int)(w * 14 / 15)));
+                            }
+                            else
+                            {
+                                if (c > th321) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 13 / 15), (int)(w * 13 / 15), (int)(w * 13 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 12 / 15), (int)(w * 12 / 15), (int)(w * 12 / 15)));
+                            }
+                        }
+                        else
+                        {
+                            if (c > th31)
+                            {
+                                if (c > th312) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 11 / 15), (int)(w * 11 / 15), (int)(w * 11 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 10 / 15), (int)(w * 10 / 15), (int)(w * 10 / 15)));
+                            }
+                            else
+                            {
+                                if (c > th311) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 9 / 15), (int)(w * 9 / 15), (int)(w * 9 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 8 / 15), (int)(w * 8 / 15), (int)(w * 8 / 15)));
+                            }
+                        }
                     }
                     else
                     {
-                        if (c > th2) tmp.SetPixel(x, y, Color.FromArgb((int)(w / 3), (int)(w / 3), (int)(w / 3)));
-                        else tmp.SetPixel(x, y, Color.FromArgb((int)b, (int)b, (int)b));
+                        if (c > th2)
+                        {
+                            if (c > th22)
+                            {
+                                if (c > th222) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 7 / 15), (int)(w * 7 / 15), (int)(w * 7 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 6 / 15), (int)(w * 6 / 15), (int)(w * 6 / 15)));
+                            }
+                            else
+                            {
+                                if (c > th221) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 5 / 15), (int)(w * 5 / 15), (int)(w * 5 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 4 / 15), (int)(w * 4 / 15), (int)(w * 4 / 15)));
+                            }
+                        }
+                        else
+                        {
+                            if (c > th21)
+                            {
+                                if (c > th212) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 3 / 15), (int)(w * 3 / 15), (int)(w * 3 / 15)));
+                                else tmp.SetPixel(x, y, Color.FromArgb((int)(w * 2 / 15), (int)(w * 2 / 15), (int)(w * 2 / 15)));
+                            }
+                            else
+                            {
+                                if (c > th211) tmp.SetPixel(x, y, Color.FromArgb((int)(w * 1 / 15), (int)(w * 1 / 15), (int)(w * 1 / 15)));
+                                tmp.SetPixel(x, y, Color.FromArgb((int)b, (int)b, (int)b));
+                            }
+                        }
                     }
                 }
             }
@@ -253,11 +317,16 @@ namespace Dithering
 
         private void loadFilter_Click(object sender, EventArgs e)
         {
-            if(random.Checked)
+            int k;
+            k = Int32.Parse(kBox.Text);
+
+            if (random.Checked)
             {
-                int k;
-                k = Int32.Parse(kBox.Text);
                 pictureBox1.Image = randomFilter(k);
+            }
+            if(error.Checked)
+            {
+                
             }
         }
     }
